15e6fc7b4cdab42374e3e9173c48583d28ea9c2b
Vik Paruchuri <vik@edx.org>
2013-02-26 18:02:12 -0500

    Work on actual api models

--- freeform_data/api.py
+++ freeform_data/api.py
@@ -1,1 +1,34 @@
-__author__ = 'vik'
+from tastypie.resources import ModelResource
+from freeform_data.models import Organization, UserProfile, Course, Problem, Essay, EssayGrade
+from django.contrib.auth import User
+
+
+class OrganizationResource(ModelResource):
+    class Meta:
+        queryset = Organization.objects.all()
+        resource_name = 'organization'
+
+class UserProfileResource(ModelResource):
+    class Meta:
+        queryset = UserProfile.objects.all()
+        resource_name = 'user_profile'
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'course'
+
+class ProblemResource(ModelResource):
+    class Meta:
+        queryset = Problem.objects.all()
+        resource_name = 'problem'
+
+class EssayResource(ModelResource):
+    class Meta:
+        queryset = Essay.objects.all()
+        resource_name = 'essay'
+
+class EssayGradeResource(ModelResource):
+    class Meta:
+        queryset = EssayGrade.objects.all()
+        resource_name = 'essay_grade'
