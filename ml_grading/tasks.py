cafbc1329d2702cfe31c74c36d53d85bcfcd86a8
Vik Paruchuri <vik@edx.org>
2013-02-26 20:08:19 -0500

    Add in some celery tasks for ml model creation

---
+++ ml_grading/tasks.py
@@ -0,0 +1,19 @@
+from celery import task
+import logging
+from celery.task import periodic_task
+from freeform_data.models import Problem
+from datetime import timedelta
+from django.conf import settings
+from ml_grading.ml_model_creation import handle_single_location
+
+log=logging.getLogger(__name__)
+
+@periodic_task(run_every=timedelta(seconds=TIME_BETWEEN_ML_CREATOR_CHECKS))
+def create_ml_models():
+    problems = Problem.objects.all()
+    for problem in problems:
+        create_ml_models_single_problem(problem)
+
+@task()
+def create_ml_models_single_problem():
+    handle_single_location(problem)
