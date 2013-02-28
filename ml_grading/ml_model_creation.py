5caabf5e3e8c8ecb51fbfb12d507387c8d20b49f
Vik Paruchuri <vik@edx.org>
2013-02-28 13:18:43 -0500

    Documentation for ml model creator

--- ml_grading/ml_model_creation.py
+++ ml_grading/ml_model_creation.py
@@ -1,3 +1,7 @@
+"""
+Scripts to generate a machine learning model from input data
+"""
+
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
 from django.utils import timezone
@@ -21,44 +25,63 @@ MAX_ESSAYS_TO_TRAIN_WITH = 1000
 MIN_ESSAYS_TO_TRAIN_WITH = 10
 
 def handle_single_problem(problem):
+    """
+    Creates a machine learning model for a given problem.
+    problem - A Problem instance (django model)
+    """
+    #This function is called by celery.  This ensures that the database is not stuck in an old transaction
     transaction.commit_unless_managed()
+    #Get prompt and essays from problem (needed to train a model)
     prompt = problem.prompt
     essays = problem.essay_set.filter(essay_type="train")
+
+    #Now, try to decode the grades from the essaygrade objects
     essay_text = []
     essay_grades = []
     essay_text_vals = essays.values('essay_text')
     for i in xrange(0,len(essays)):
         try:
+            #Get an instructor score for a given essay (stored as a json string in DB) and convert to a list.  Looks like [1,1]
+            #where each number denotes a score for a given target number
             essay_grades.append(json.loads(essays[i].get_instructor_scored()[0].target_scores))
+            #If a grade could successfully be found, then add the essay text.  Both lists need to be in sync.
             essay_text.append(essay_text_vals[i]['essay_text'])
         except:
             log.exception("Could not get latest instructor scored for {0}".format(essays[i]))
 
     try:
+        #This is needed to remove stray characters that could break the machine learning code
         essay_text = [et.encode('ascii', 'ignore') for et in essay_text]
     except:
         error_message = "Could not correctly encode some submissions: {0}".format(essay_text)
         log.exception(error_message)
         return False, error_message
 
+    #Get the maximum target scores from the problem
     first_len = len(json.loads(problem.max_target_scores))
     for i in xrange(0,len(essay_grades)):
+        #All of the lists within the essay grade list (ie [[[1,1],[2,2]]) need to be the same length
         if len(essay_grades[i])!=first_len:
             error_message = "Problem with an instructor scored essay! {0}".format(essay_grades)
             log.exception(error_message)
             return False, error_message
 
+    #Too many essays can take a very long time to train and eat up system resources.  Enforce a max.
+    # Accuracy increases logarithmically, anyways, so you dont lose much here.
     if len(essay_text)>MAX_ESSAYS_TO_TRAIN_WITH:
         essay_text = essay_text[:MAX_ESSAYS_TO_TRAIN_WITH]
         essay_grades = essay_grades[:MAX_ESSAYS_TO_TRAIN_WITH]
 
     graded_sub_count = len(essay_text)
+    #If there are too few essays, then don't train a model.  Need a minimum to get any kind of accuracy.
     if graded_sub_count < MIN_ESSAYS_TO_TRAIN_WITH:
         error_message = "Too few too create a model for problem {0}  need {1} only have {2}".format(problem, MIN_ESSAYS_TO_TRAIN_WITH, graded_sub_count)
         log.error(error_message)
         return False, error_message
 
+    #Loops through each potential target
     for m in xrange(0,first_len):
+        #Gets all of the scores for this particular target
         scores = [s[m] for s in essay_grades]
         max_score = max(scores)
         log.debug("Currently on location {0} in problem {1}".format(m, problem.id))
@@ -89,7 +112,7 @@ def handle_single_problem(problem):
                         problem_id, settings.TIME_BEFORE_REMOVING_STARTED_MODEL))
                     created_model.delete()
                     model_started = False
-
+            #If a model has not been started, then initialize an entry in the database to prevent other threads from duplicating work
             if not model_started:
                 created_model_dict_initial={
                     'max_score' : max_score,
@@ -113,6 +136,7 @@ def handle_single_problem(problem):
                         prompt = ""
                 prompt = prompt.encode('ascii', 'ignore')
 
+                #Call on the machine-learning repo to create a model
                 results = create.create(essay_text, scores, prompt)
 
                 scores = [int(score_item) for score_item in scores]
@@ -161,6 +185,11 @@ def handle_single_problem(problem):
     return True, "Creation succeeded."
 
 def save_model_file(results, save_to_s3):
+    """
+    Saves a machine learning model to file or uploads to S3 as needed
+    results - Dictionary of results from ML
+    save_to_s3 - Boolean indicating whether or not to upload results
+    """
     success=False
     if save_to_s3:
         pickled_model=ml_grading_util.get_pickle_data(results['prompt'], results['feature_ext'],
