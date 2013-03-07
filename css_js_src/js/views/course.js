d4c4586008db099d2503257bd269ad4e9de34ea0
Vik Paruchuri <vik@edx.org>
2013-03-06 19:15:31 -0500

    Work on backbone views

--- css_js_src/js/views/course.js
+++ css_js_src/js/views/course.js
@@ -5,7 +5,7 @@ CourseView = Backbone.View.extend({
         // one request at a time
         this.isLoading = false;
         this.courseList = new CourseList();
-        _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+        _.bindAll(this, 'render', 'loadResults'); // fixes loss of context for 'this' within methods
         this.render(); // not all views are self-rendering. This one is.
     },
     render: function () {
@@ -25,18 +25,86 @@ CourseView = Backbone.View.extend({
             }
         });
     },
-    // This will simply listen for scroll events on the current el
-    events: {
-        'scroll': 'checkScroll'
+});
+
+var CourseItemView = Backbone.View.extend({
+    tagName: 'li', // name of tag to be created
+    initialize: function () {
+        // isLoading is a useful flag to make sure we don't send off more than
+        // one request at a time
+        _.bindAll(this, 'render', 'unrender', 'swap', 'remove', 'loadResults'); // every function that uses 'this' as the current object should be in here
+        this.model.bind('remove', this.unrender);
     },
-    checkScroll: function () {
-        var triggerPoint = 100; // 100px from the bottom
-        if( !this.isLoading && this.el.scrollTop + this.el.clientHeight + triggerPoint > this.el.scrollHeight ) {
-            this.courseCollection.page += 1; // Load next page
-            this.loadResults();
-        }
+    events: {
+        'click span.swap':  'swap',
+        'click span.delete': 'remove'
+    },
+    initialize: function(){
+        _.bindAll(this, 'render', 'unrender', 'swap', 'remove', 'loadResults'); // every function that uses 'this' as the current object should be in here
+        this.model.bind('remove', this.unrender);
+    },
+    render: function () {
+        return(_.template($('#course-list-template').html(), {courses: this.model, _:_}));
+    },
+    unrender: function(){
+        $(this.el).remove();
+    },
+    remove: function(){
+        this.model.destroy();
     }
 });
+
+var CourseListView = Backbone.View.extend({
+    el: '.course-view', // el attaches to existing element
+    events: {
+        'click button#add': 'addItem'
+    },
+    loadResults: function () {
+        var that = this;
+        // we are starting a new load of results so set isLoading to true
+        this.isLoading = true;
+        // fetch is Backbone.js native function for calling and parsing the collection url
+        this.courseList.fetch({
+            success: function (courses) {
+                // Once the results are returned lets populate our template
+                that.appendItem(courses.models);
+                // Now we have finished loading set isLoading back to false
+                that.isLoading = false;
+            }
+        });
+    },
+    initialize: function(){
+        _.bindAll(this, 'render', 'addItem', 'appendItem'); // every function that uses 'this' as the current object should be in here
+
+        this.isLoading = false;
+        this.courseList = new CourseList();
+        this.courseList.bind('add', this.appendItem); // collection event binder
+
+        this.counter = 0;
+        this.render();
+    },
+    render: function(){
+        var self = this;
+        $(this.el).append("<button id='Add'>Add list item</button>");
+        $(this.el).append("<ul></ul>");
+        this.courseList.fetch()
+        _(this.courseList.models).each(function(item){ // in case collection is not empty
+            this.appendItem(item);
+        }, this);
+    },
+    addItem: function(){
+        this.counter++;
+        var courseItem = new CourseItem();
+        this.courseList.add(courseItem);
+    },
+    appendItem: function(item){
+        var courseItemView = new CourseItemView({
+            model: item
+        });
+        $(this.el).append(courseItemView.render());
+    }
+});
+
 
 $(document).ready(function() {
     course_view = new CourseView()
