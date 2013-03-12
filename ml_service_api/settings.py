0881561b420ae1a940bfd59dd70a9c03a92bce87
Vik Paruchuri <vik@edx.org>
2013-03-12 15:59:27 -0400

    Add in backbone extension and custom serializer

--- ml_service_api/settings.py
+++ ml_service_api/settings.py
@@ -16,7 +16,7 @@ ENV_ROOT = REPO_PATH.dirname()
 
 #ML Specific settings
 ML_PATH = os.path.join(ENV_ROOT, "machine-learning/") #Path to ML repo containing grade.py and create.py
-ML_MODEL_PATH=os.path.join(ENV_ROOT,"ml_models/") #Path to save and retrieve ML models from
+ML_MODEL_PATH=os.path.join(ENV_ROOT,"ml_models_api/") #Path to save and retrieve ML models from
 TIME_BETWEEN_ML_CREATOR_CHECKS= 1 * 60 # seconds.  Time between ML creator checking to see if models need to be made.
 TIME_BETWEEN_ML_GRADER_CHECKS= 10 # seconds.  Time between ML grader checking to see if models need to be made.
 USE_S3_TO_STORE_MODELS= False #Determines whether or not models are placed in Amazon S3
@@ -104,7 +104,7 @@ PIPELINE_JS = {
             'js/json2.js',
             'js/underscore.js',
             'js/backbone.js',
-            'js/extend_backbone.js',
+            'js/backbone-tastypie.js',
             'js/jquery.cookie.js',
             ],
         'output_filename': 'js/util.js',
