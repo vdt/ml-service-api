6361ad1f8cb90526149efcf1136e3f4135f51ac4
Vik Paruchuri <vik@edx.org>
2013-03-12 17:13:40 -0400

    Autodiscover models from schema

--- css_js_src/js/views/course.js
+++ css_js_src/js/views/course.js
@@ -1,102 +1,100 @@
-CourseListView = Backbone.View.extend({
-    tagName: "ul",
-    el: '.course-view',
-    initialize: function(){
-        _.bindAll(this, "renderItem", "loadResults");
-        this.courseList = new CourseList();
-    },
+waitForSchema(function() {
+    CourseListView = Backbone.View.extend({
+        tagName: "ul",
+        el: '.course-view',
+        initialize: function(){
+            _.bindAll(this, "renderItem", "loadResults");
+            this.courseList = new CourseContainer();
+        },
 
-    renderItem: function(model){
-        var courseItemView = new CourseItemView({model: model});
-        courseItemView.render();
-        $(this.el).append(courseItemView.el);
-    },
+        renderItem: function(model){
+            var courseItemView = new CourseItemView({model: model});
+            courseItemView.render();
+            $(this.el).append(courseItemView.el);
+        },
 
-    render: function(){
-        this.loadResults();
-    },
+        render: function(){
+            this.loadResults();
+        },
 
-    loadResults: function () {
-        var that = this;
-        // we are starting a new load of results so set isLoading to true
-        this.isLoading = true;
-        // fetch is Backbone.js native function for calling and parsing the collection url
-        this.courseList.fetch({
-            success: function (courses) {
-                // Once the results are returned lets populate our template
-                console.log(courses.models);
-                for(model in courses.models)
+        loadResults: function () {
+            var that = this;
+            // we are starting a new load of results so set isLoading to true
+            this.isLoading = true;
+            // fetch is Backbone.js native function for calling and parsing the collection url
+            this.courseList.fetch({
+                success: function (courses) {
+                    // Once the results are returned lets populate our template
+                    console.log(courses.models);
+                    for(model in courses.models)
+                    {
+                        that.renderItem(courses.models[model])
+                    }
+                }
+            });
+        }
+    });
+
+    CourseItemView = Backbone.View.extend({
+        tagName: "li",
+        events: {
+            "click a#show": "clicked",
+            "click #update": "update",
+            "click #add": "update"
+        },
+
+        initialize: function(){
+            _.bindAll(this, "clicked", "render", "update");
+        },
+
+        clicked: function(e){
+            e.preventDefault();
+            var detail_shown = $(this.el).data('detail')
+            if(detail_shown){
+                $(this.el).children("#item-detail").remove()
+                $(this.el).children("#item-add").remove()
+                $(this.el).data('detail', false)
+            } else {
+                var item_template = $('#course-item-detail-template');
+                var html = (_.template(item_template.html(), {course: this.model, _:_}));
+                var update_template = $('#course-item-add-template');
+                var update_html = (_.template(update_template.html(), {course: this.model.toJSON(), _:_}));
+                $(this.el).append(html);
+                $(this.el).append(update_html)
+                $(this.el).data('detail', true)
+            }
+        },
+
+        render: function(){
+            var item_template = $('#course-item-template');
+            var html = (_.template(item_template.html(), {course: this.model, _:_}));
+            $(this.el).append(html);
+            $(this.el).data('detail', false)
+        },
+
+        update: function() {
+            var update_elements = $(this.el).children('#item-add').children('.update');
+            var update_dict = {};
+            for(var el in update_elements)
+            {
+                var id = update_elements[el].id
+                var value=update_elements[el].value;
+                try {
+                    value = JSON.parse(value);
+                }
+                catch(err)
                 {
-                    that.renderItem(courses.models[model])
+
+                }
+                if(id!="id" && id!="" && value!="" && id!="modified" && id!="created" && id!="resource_uri")
+                {
+                    update_dict[id] = value;
                 }
             }
-        });
-    }
-});
-
-CourseItemView = Backbone.View.extend({
-    tagName: "li",
-    events: {
-        "click a#show": "clicked",
-        "click #update": "update"
-    },
-
-    initialize: function(){
-        _.bindAll(this, "clicked", "render", "update");
-    },
-
-    clicked: function(e){
-        e.preventDefault();
-        var detail_shown = $(this.el).data('detail')
-        if(detail_shown){
-            $(this.el).children("#item-detail").remove()
-            $(this.el).children("#item-add").remove()
-            $(this.el).data('detail', false)
-        } else {
-            var item_template = $('#course-item-detail-template');
-            var html = (_.template(item_template.html(), {course: this.model, _:_}));
-            var update_template = $('#course-item-add-template');
-            console.log(this.model.toJSON());
-            var update_html = (_.template(update_template.html(), {course: this.model.toJSON(), _:_}));
-            $(this.el).append(html);
-            $(this.el).append(update_html)
-            $(this.el).data('detail', true)
+            this.model.save(update_dict, {patch: true});
         }
-    },
+    });
 
-    render: function(){
-        var item_template = $('#course-item-template');
-        var html = (_.template(item_template.html(), {course: this.model, _:_}));
-        $(this.el).append(html);
-        $(this.el).data('detail', false)
-    },
-
-    update: function() {
-        var update_elements = $(this.el).children('#item-add').children('.update');
-        var update_dict = {};
-        for(var el in update_elements)
-        {
-            var id = update_elements[el].id
-            var value=update_elements[el].value;
-            try {
-                value = JSON.parse(value);
-            }
-            catch(err)
-            {
-                
-            }
-            console.log(value)
-            if(id!="id" && id!="" && value!="" && id!="modified" && id!="created" && id!="resource_uri")
-            {
-                update_dict[id] = value;
-            }
-        }
-        console.log(update_dict);
-        this.model.save(update_dict, {patch: true});
-    }
-});
-
-$(document).ready(function() {
     var view = new CourseListView();
     view.render();
 });
